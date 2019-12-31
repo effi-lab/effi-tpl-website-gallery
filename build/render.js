da099205ed3bfb10af8220c3d1a362b5d644ab13
Andrew Zhang <zhangmhao@gmail.com>
2019-12-31 18:10:57 +0800

    feat: read from rss

--- build/render.js
+++ build/render.js
@@ -8,31 +8,41 @@ function readTpl(tplName) {
   return path.resolve(currentWorkDirectory, `./tpl/${tplName}`);
 }
 
-function readPageData() {
-  const dataPath = path.resolve(currentWorkDirectory, './data/podcasts.toml');
+function readTomlData(filePath) {
+  const dataPath = path.resolve(currentWorkDirectory, filePath);
   const tomlContent = fs.readFileSync(dataPath);
   return toml.parse(tomlContent);
 }
 
-function genHtml(pageData) {
+function readSiteData() {
+  return readTomlData('./data/site.toml');
+}
+
+function readContentData() {
+  return readTomlData('./data/content.toml');
+}
+
+function genHtml(siteData, listData) {
   const tplPath = readTpl('index.mustache');
   const tplContent = fs.readFileSync(tplPath, 'utf-8');
-  const renderedHtml = Mustache.render(tplContent, pageData);
+  const renderedHtml = Mustache.render(tplContent, { site: siteData, content: listData });
   // create index.html
   const indexPath = path.resolve(currentWorkDirectory, './index.html');
   fs.writeFileSync(indexPath, renderedHtml);
 }
 
-function genReadme(pageData) {
+function genReadme(siteData, listData) {
   const tplPath = readTpl('readme.mustache');
   const tplContent = fs.readFileSync(tplPath, 'utf-8');
-  const renderedContent = Mustache.render(tplContent, pageData);
+  const renderedContent = Mustache.render(tplContent, { site: siteData, content: listData });
   // create readme.md
   fs.writeFileSync(path.resolve(currentWorkDirectory, './readme.md'), renderedContent);
 }
 
 
-const pageData = readPageData();
+const siteData = readSiteData();
+const content = readContentData();
 
-genHtml(pageData);
-genReadme(pageData);
+console.log(content);
+genHtml(siteData, content);
+genReadme(siteData, content);
